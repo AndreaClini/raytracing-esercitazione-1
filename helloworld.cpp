2b81a50d83592a40097a0c7b4a28f986cbbffd78
AndreaClini <andreaclini96@gmail.com>
2025-02-26 11:54:23 +0100

    First release (with typo)

---
+++ helloworld.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    
+    //uncomment this line and substitute it to the previous one to get a name as argument
+    //int main(int argc, char* argv[]) { 
+
+    // Default message
+    std::cout << "Hello Wold";  //there is a typo!
+
+    // // If a name is provided as a command-line argument, append it
+    
+    // if (argc > 1) {
+    //     std::cout << " " << argv[1];
+    // }
+
+    std::cout << "!" << std::endl;
+    return 0;
+}
